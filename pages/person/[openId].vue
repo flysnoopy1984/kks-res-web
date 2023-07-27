b75ae99971d9dc85f1f531f44c6446a783063371
jacky.song <song.fuwei@hotmail.com>
2023-07-27 17:45:52 +0800

    wx

--- pages/person/[openId].vue
+++ pages/person/[openId].vue
@@ -2,41 +2,43 @@
     <div>
         <!-- {{ $route.params.group }}
         {{ $route.params.openId }} -->
-        <div class="userInfoContainer" v-show="userInfo.openId!=null">
-          <p>
-            <n-avatar
-              size="large"
-              :src=userInfo.headImgUrl
-            /></p>
-          <p>nickname: {{userInfo.nickName}}</p>
-          <p>sex: {{userInfo.sex}}</p>
-          <p>city: {{userInfo.city }}</p>
-          <p>province: {{userInfo.province}}</p>
-      </div>
-      <div>
-         
-      </div>
+        <div class="userInfoContainer" v-if="userInfo!=undefined">
+            <p>
+              <n-avatar
+                size="large"
+                :src=userInfo.headImgUrl
+              /></p>
+            <p>nickname: {{userInfo.nickName}}</p>
+            <p>sex: {{userInfo.sex}}</p>
+            <p>city: {{userInfo.city }}</p>
+            <p>province: {{userInfo.province}}</p>
+            <div>
+                <n-button type="primary" @click="nav.toHome()">返回主页</n-button>
+            </div>
+        </div>
+        <div v-else>
+          <n-button type="primary" @click="nav.tojsTest()">返回登陆</n-button>
+        </div>
+     
+     
+     
     </div>
 </template>
 <script setup>
 import { NAvatar } from 'naive-ui'
+import { NButton } from 'naive-ui'
 definePageMeta({
-    layout: 'person'
+    layout: 'person',
+    
 })
 
+ const userInfo = useUserInfo().value; 
+// if(userInfo == undefined){
+//     nav.toWxLogin();
+// }
 
 
-const userInfo = useUserInfo().value; 
 
-
-
-
-console.log("openId",userInfo.openId);
-
-
-function backToHome(){
-    navigateTo("/");
-}
 </script>
 <style scoped>
 .userInfoContainer{
