5883802af7b8e84e2354cab9a28c3447e5ae88f0
jacky.song <song.fuwei@hotmail.com>
2023-08-25 17:43:27 +0800

    图片处理

---
+++ pages/my/imgTest.vue
@@ -0,0 +1,18 @@
+<!--
+created by JackySong@2023
+-->
+<template>
+    <div>
+        <n-image
+    lazy
+    width="100"
+    src="http://iqianba.cn/img/s/1.jpg"
+  />
+    </div>
+</template>
+
+<script setup>
+import { NImage } from 'naive-ui'
+</script>
+<style scoped>
+</style>
