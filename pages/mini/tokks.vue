130ad0b309018afe322c120bb324f2d71606fb40
jackysong2020 <song_fuwei@126.com>
2024-05-22 16:33:44 +0800

    页面跳转

--- pages/mini/tokks.vue
+++ pages/mini/tokks.vue
@@ -3,12 +3,20 @@ created by JackySong@2023
 -->
 
 <template>
-    <div>
-        <n-spin size="large"  class="loadingArea">
-            <!-- <template #description>
-                
-            </template> -->
-        </n-spin> 
+    <div class="mainContainer">
+        <div class="card">
+            <div class="signHeader">
+                <a class="logo" ></a>
+                <div class="titleRow">
+                    <div class="title">卡卡送 </div>
+                     <div class="web-des">为您提供最贴心的祝福</div>
+                     
+                </div>
+            </div>
+            <n-button class="btnLogin" type="info" @click="toMiniApp">
+                进入
+            </n-button>
+        </div>
     </div>
 </template>
 
@@ -16,12 +24,27 @@ created by JackySong@2023
 definePageMeta({
     layout:'blank'
 })
-import {NSpin } from 'naive-ui';
+import {NSpin,NButton } from 'naive-ui';
 import apiMini from '@/zfApi/apiMini';
 
+
+let req ;
+let toUrl = undefined;
 init();
 
 //test Url:"weixin://dl/business/?t=NODlKue4yai"
+
+function toMiniApp(){
+    // debugger
+    if(toUrl == undefined || toUrl=="" )
+        init();
+    else{
+        if(process.client){
+            window.location.href =toUrl;
+        }      
+    }
+    
+}
 
 function init(){
     const route = useRoute();
@@ -29,7 +52,7 @@ function init(){
     const posterId =route.query.posterId;
 
     if(posterId !=undefined){
-            let req = {}
+          
             if(posterId == "home"){
                 req = {
                     "jump_wxa":
@@ -40,17 +63,17 @@ function init(){
                     }
                 }
             }
-            else if(posterId = "section"){
+            else if(posterId == "section"){
                 req = {
                     "jump_wxa":
                     {
                         "path": "/pages/home/home_section",
-                        "query": "sectionCode=dailyInter&sectionName=日常互动",
+                        "query": encodeURI("sectionCode=dailyInter&sectionName=日常互动"),
                         "env_version": "release"
                     }
                 }
             }
-            else if(posterId = "calendar"){
+            else if(posterId == "calendar"){
                 req = {
                     "jump_wxa":
                     {
@@ -70,14 +93,13 @@ function init(){
                     }
                 }
              }
-        
+        debugger
              reqSchemeUrl(req);
              return;
           
     }
 
     if(process.client){
- 
         if(scheme !=undefined){
             window.location.href =scheme;
             return;
@@ -87,12 +109,13 @@ function init(){
        
 }
 
-async function reqSchemeUrl(req){
-
+async function reqSchemeUrl(){
+// debugger
     const res = await apiMini.genUrlScheme(req);
              console.log("res:",res);
-    debugger
+
     if(process.client){
+        toUrl = res.data.openlink;
         window.location.href =res.data.openlink;
     }       
     return;
@@ -101,8 +124,75 @@ async function reqSchemeUrl(req){
 
 </script>
 <style scoped>
+@font-face{
+	font-family: 'aliHealth';
+	/* src:url("../fonts/aliHealth-kakasong.ttf"); */
+	src:url("@/assets/fonts/kks-aliHealth.ttf");
+}
+@font-face{
+	font-family: 'bxz';
+	src:url("@/assets/fonts/kks-biaoxiaozhi.ttf");
+}
+
+.mainContainer{
+    margin:20px auto;
+    display: flex;
+    justify-content: center;
+}
+.card{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.signHeader{
+    display: flex;
+     justify-content: center;
+    
+     /* margin-top: 20px; */
+}
+.signHeader .logo{
+    display:inline-block;
+    height: 80px;
+    width: 100px;
+    background: url('/img/kksLogo_128.png')  no-repeat;
+    background-size: 80px 80px;
+}
+.titleRow{
+    /* align-self:center; */
+    display: flex; 
+    flex-direction: column;
+    justify-content: space-around;
+
+}
+.title{
+  
+    font-size: 20px;
+    font-weight: 700; 
+    font-family: 'aliHealth';
+    color: #4441A9;
+    margin-top:8px;
+    /* font-family: "aliHealth", */
+}
+
+.web-des{
+	font-family: 'bxz';
+	letter-spacing: 2px;
+	font-size: 14px;
+	margin-left: 2px;
+    margin-right: 10px;
+	color: #61a9b9;
+	padding-top: 6px;
+}
+
+
+
 .loadingArea{
     height:400px;
-    
+    width: 100%;
+
+}
+.btnLogin{
+    margin-top:40px;
+    width: 120px;
 }
 </style>
